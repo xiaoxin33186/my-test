9e6552e8c45ba40b580f559be7ef227952119520
xiaoxin33186 <331863229@qq.com:git config --global user.email 331863229@qq.com>
2017-06-21 16:20:50 +0800

    测试冲突添加dev1

--- wechat-mini/src/test/java/com/ddjf/BaseTestCase.java
+++ wechat-mini/src/test/java/com/ddjf/BaseTestCase.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath*:/applicationContext.xml"})
+System.out.println("div2test");
 public class BaseTestCase {
 	
 }
